8239adc4176a8227390fda26ff34d7c20479ca35
Elena Domínguez Jaén <elenadmguezjaen@gmail.com>
2025-02-23 00:15:14 +0100

    header hover y logo

--- scripts/components.js
+++ scripts/components.js
@@ -9,6 +9,23 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("link-cv").href = pathPrefix + "cv.html"; // Ruta del CV
         document.getElementById("link-contact").href = pathPrefix + "contact.html"; // Ruta de Contacto
     }, 100);
+
+    // Cambiar la imagen del logo al hacer hover
+    const logoImage = document.querySelector(".navbar .logo img");
+    const defaultImage = 'images/elena-dominguez-texto.png'; // Imagen por defecto
+    const hoverImage = 'images/elena-dominguez-texto-hover.png'; // Imagen cuando el ratón entra
+
+    if (logoImage) {
+        logoImage.src = defaultImage; // Aseguramos que la imagen por defecto esté cargada
+
+        logoImage.addEventListener("mouseenter", function() {
+            logoImage.src = hoverImage; // Cambiar imagen cuando el ratón entra
+        });
+
+        logoImage.addEventListener("mouseleave", function() {
+            logoImage.src = defaultImage; // Volver a la imagen por defecto cuando el ratón sale
+        });
+    }
 });
 
 function loadComponent(id, file) {
