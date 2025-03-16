bf0151088169326b7d0bb2509e9693bb34360a90
Cesar Almeida <cesaralmeida89@gmail.com>
2025-03-16 16:36:32 +0100

    Add burger icon

--- src/scripts/burger-menu.js
+++ src/scripts/burger-menu.js
@@ -2,13 +2,16 @@
 const burgerMenuBtn = document.querySelector(".burger-menu");
 const navLeft = document.querySelector(".nav-left");
 const navRigth = document.querySelector(".nav-right");
+const burgerIcon = document.querySelector(".burger-icon");
 
 burgerMenuBtn.addEventListener("click", function () {
     if (navLeft.style.display === "flex") {
         navLeft.style.display = "none";
         navRigth.style.display = "none";
+        burgerIcon.innerHTML = "&#9776;";
     } else {
         navLeft.style.display = "flex";
         navRigth.style.display = "flex";
+        burgerIcon.innerHTML = "&times;";
     }
 });
