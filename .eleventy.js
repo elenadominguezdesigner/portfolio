93620408bb7193452d80322cdd3bffebe635557b
Elena Domínguez Jaén <elenadmguezjaen@gmail.com>
2025-03-30 09:32:59 +0200

    Fix pathprefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,27 @@
-module.exports = function (eleventyConfig) {
-eleventyConfig.addPassthroughCopy("./src/images/");
-eleventyConfig.addPassthroughCopy("./src/css/");
-eleventyConfig.addPassthroughCopy("./src/scripts/");
+module.exports = async function (eleventyConfig) {
+    const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
 
-eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
+        pathPrefix: "/portfolio/"
+    });
 
-eleventyConfig.addCollection("orderedPages", function(collectionApi) {
-    return collectionApi.getAll().filter(page => !page.inputPath.endsWith("index.md")).sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
-});
+    eleventyConfig.addPassthroughCopy("./src/images/");
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/scripts/");
+    eleventyConfig.addWatchTarget("./src/css/");
+
+    eleventyConfig.addCollection("orderedPages", function(collectionApi) {
+        return collectionApi.getAll()
+            .filter(page => !page.inputPath.endsWith("index.md"))
+            .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+    });
 
     return {
         dir: {
             input: "src",
             output: "docs"
         },
+        pathPrefix: "/portfolio/",
         serverOptions: {
             showAllHosts: true
         }
